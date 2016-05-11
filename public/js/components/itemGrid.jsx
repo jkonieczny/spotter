163fc29d54767686b7b23d377171856f9ae865c8
dalyr95 <unknown9595@hotmail.com>
2016-05-11 12:45:38 +0100

    Added email, bold font and serviceWorker

---
+++ public/js/components/itemGrid.jsx
@@ -0,0 +1,82 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+var React = require('react'),
+	Fluxxor = require('fluxxor'),
+	FluxMixin = Fluxxor.FluxMixin(React),
+    StoreWatchMixin = Fluxxor.StoreWatchMixin;
+
+var cx = require('classnames');
+
+module.exports = React.createClass({
+    displayName: 'itemGrid.jsx',
+	mixins: [FluxMixin, StoreWatchMixin('UserStore', 'ProductStore')],
+	getInitialState: function() {
+		return {};
+	},
+	getStateFromFlux: function() {
+		return {
+            selectedProducts: this.getFlux().store('ProductStore').getState().selectedProducts
+        };
+	},
+    render: function() {
+        if (this.state.selectedProducts.length > 0) {
+            var selectedProducts = [];
+
+            if (this.state.selectedProducts.length > 0) {
+                this.state.selectedProducts.forEach(function(product) {
+                    var randomFluxPrice = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+                    var topPrice = product.price * 1.18 + randomFluxPrice;
+
+                    selectedProducts.push(
+                        (
+                            <div key={ product.id } className="item_grid center">
+                                <div className="item_grid_img" style={{backgroundImage: 'url(' + product.image + '), url(images/icon.png)'}}>
+                                    <div className="item_grid_saving">
+                                        { Math.round((topPrice - product.price) / topPrice * 100) }
+                                        <sup>%</sup>
+                                    </div>
+                                </div>
+                                {product.name}
+                                <div className="item_price">&pound;{product.price.toFixed(2)}</div>
+                                <button onClick={ this.proceedBuy }>Buy</button>
+                                <ul className="item_grid_others left">
+                                    <li>
+                                        <a href="#" onClick={ this.proceedBuy }>
+                                            <span>Discount Nutrition</span>
+                                            <span>&pound;{ (product.price * 1.08).toFixed(2)}</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" onClick={ this.proceedBuy }>
+                                            <span>Protein Online</span>
+                                            <span>&pound;{ (product.price * 1.12).toFixed(2)}</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" onClick={ this.proceedBuy }>
+                                            <span>BuySupplements</span>
+                                            <span>&pound;{ topPrice.toFixed(2)}</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        )
+                    );
+                }.bind(this));
+            }
+        }
+
+        return (
+            <div className="item_grid_parent">
+                { selectedProducts }
+            </div>
+        );
+    },
+    proceedBuy: function(e) {
+        e.preventDefault();
+        console.log('proceedBuy');
+    }
+
+});
---
+++ public/serviceWorker.js
@@ -0,0 +1,61 @@
+var CURRENT_CACHES = 'v1.0.6';
+
+self.addEventListener('install', function(event) {
+  console.log("SW installed", CURRENT_CACHES, self.registration.scope);
+  var cachedAssests = [
+    '/',
+    '/spotter',
+    'index.html',
+    'bundle/css/bundle.css',
+    'bundle/js/bundle.js',
+    'images/fit-avatar.png',
+    'images/icon.png',
+    'images/avatars/arnold.jpg',
+    'images/avatars/jan.jpg',
+    'images/avatars/julia.jpg',
+    'images/avatars/rob.jpg',
+    'images/avatars/trainer.jpg',
+    'images/avatars/trainer-large.jpg',
+    'fonts/AvenirNext-Regular.woff2',
+    'fonts/AvenirNext-Bold.woff2'
+  ];
+
+  // Node removes trailing slashes, Apache addes them!
+  var spotterDir = (self.registration.scope.search('localhost') > - 1) ? '/spotter' : '/spotter/';
+  cachedAssests.push(spotterDir);
+
+  event.waitUntil(
+    caches.open(CURRENT_CACHES).then(function(cache) {
+      return cache.addAll(cachedAssests);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('SW activated');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (CURRENT_CACHES.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Hello world
