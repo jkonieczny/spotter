5e1c9bad0ea242ffc547571aa286ab890ebbfeb6
Robert Daly <unknown9595@hotmail.com>
2017-02-22 19:18:38 +0000

    Redirect if on /page in node

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ var express = require('express'),
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+
+	if (req.originalUrl.search('/page') > -1) {
+		return res.redirect(301, '/');
+	}
+
+	return next();
+});
+
 app.use("/spotter", express.static(__dirname + "/public/index.html"));
 // Set /public as our static content dir
-app.use("/page/*", express.static(__dirname + "/public/index.html"));
 app.use("/", express.static(__dirname + "/public/"));
 
 // Fire this bitch up (start our server)
 var server = http.createServer(app).listen(port, function() {
-  console.log('Express server listening on port ' + port);
+	console.log('Express server listening on port ' + port);
 });
