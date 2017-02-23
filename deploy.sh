6adb33ac893ae9d59a04791872a10fc7acdf6913
Jan Konieczny <jkonieczny@gmail.com>
2017-02-23 11:57:26 +0000

    adding timestamp to bundle.js on deploy

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ TARGET="s3://$BUCKET/"
 
 echo "Deploying from: '$DIR' to $TARGET"
 INDEX_FILE=public/index.html
-INDEX_FILE_BACKUP_EXTENSION='.release'
+INDEX_FILE_BACKUP_EXTENSION=".release"
 INDEX_FILE_BACKUP=$INDEX_FILE$INDEX_FILE_BACKUP_EXTENSION
 TS=`date +"%s"`
-echo "Updating bundle.js url in " $FILE
+echo "Updating bundle.js url in " $INDEX_FILE
 SED_RULE='s#bundle/js/bundle.js#bundle/js/bundle.js?v='$TS'#g'
-sed -i $INDEX_FILE_BACKUP_EXTENSION $SED_RULE $FILE
+sed -i $INDEX_FILE_BACKUP_EXTENSION $SED_RULE $INDEX_FILE
 aws --profile=spotter s3 sync $DIST $TARGET
-echo "reverting back" $FILE
+echo "reverting back" $INDEX_FILE
 mv $INDEX_FILE_BACKUP $INDEX_FILE
 
